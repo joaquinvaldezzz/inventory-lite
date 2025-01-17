3fbfc4f3895da9e9d5a4266e0d3dd28d401ccee1
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-01-17 22:35:48 +0800

    refactor: capitalize filenames

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
 import { Redirect, Route } from 'react-router-dom'
 
-import Home from './pages/home'
+import Home from './pages/Home'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css'
