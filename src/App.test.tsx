2b11518d50a0bcf052038f2d820403daff7cb2f3
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-01-21 16:12:06 +0800

    fix: rename files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react'
 
-import App from './App'
+import App from './app'
 
 test('renders without crashing', () => {
   const { baseElement } = render(<App />)
