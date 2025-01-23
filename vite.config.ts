a1ca6d8bf7398e6229517cea358204a5f4ecb490
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-01-23 15:18:56 +0800

    feat: add API proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference -- Required for Vite to work with Vitest
 /// <reference types="vitest" />
 
 import path from 'path'
@@ -11,6 +12,27 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://trial.integra-payroll.com/api/login.php',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('error', err)
+          })
+          proxy.on('proxyReq', (_proxyReq, req, _res) => {
+            console.log('Request sent to target:', req.method, req.url)
+          })
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Response received from target:', proxyRes.statusCode, req.url)
+          })
+        },
+      },
     },
   },
   build: {
