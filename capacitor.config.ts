b901118249fcfbe037d1c40708d98da24ca1e349
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-01-30 15:51:19 +0800

    chore: change app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.inventorylite.app',
   appName: 'Inventory Lite',
   webDir: 'dist',
 }
