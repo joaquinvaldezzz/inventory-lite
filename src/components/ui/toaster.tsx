b3583331ec58f60a31875088d4ed527bc83fd840
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-01-30 15:05:56 +0800

    fix: check for null values

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -17,8 +17,8 @@ export function Toaster() {
         return (
           <Toast key={id} {...props}>
             <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && <ToastDescription>{description}</ToastDescription>}
+              {title != null && <ToastTitle>{title}</ToastTitle>}
+              {description != null && <ToastDescription>{description}</ToastDescription>}
             </div>
             {action}
             <ToastClose />
