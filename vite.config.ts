a276a52cde125bdee4c144c5c2b16d41099186db
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-02-01 17:48:12 +0800

    chore: install @tailwindcss/vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [legacy(), react()],
+  plugins: [legacy(), react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
