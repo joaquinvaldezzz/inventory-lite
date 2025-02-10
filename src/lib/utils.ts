1ee6c5af9f36e68189223048d57a7a430001a761
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-02-10 08:36:19 +0800

    feat: create formatCurrency utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(int: number) {
+  const PHP = new Intl.NumberFormat('en-PH', {
+    style: 'currency',
+    currency: 'PHP',
+  })
+
+  return PHP.format(int)
+}
