3b25483f92885d548ede8f11525a4db347c310e8
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-02-13 13:10:41 +0800

    feat(ui): add negative margins

--- src/components/ui/data-table/column-header.tsx
+++ src/components/ui/data-table/column-header.tsx
@@ -21,7 +21,7 @@ export function DataTableColumnHeader<TData, TValue>({
 
   return (
     <Button
-      className={cn(className)}
+      className={cn('-mx-3', className)}
       variant="ghost"
       onClick={() => {
         column.toggleSorting(column.getIsSorted() === 'asc')
