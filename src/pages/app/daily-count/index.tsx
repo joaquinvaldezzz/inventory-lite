3363ba802754916974827a990951f34323def27c
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-02-13 13:07:24 +0800

    feat(ui): configure header and toolbar styles

--- src/pages/app/daily-count/index.tsx
+++ src/pages/app/daily-count/index.tsx
@@ -1,17 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react'
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonProgressBar,
+  IonTitle,
+  IonToolbar,
+  useIonModal,
+} from '@ionic/react'
+import type { OverlayEventDetail } from '@ionic/react/dist/types/components/react-component-lib/interfaces'
 import { useQuery } from '@tanstack/react-query'
+import { add } from 'ionicons/icons'
 
 import { getDailyCountEntries } from '@/lib/api'
 
 import { columns } from './columns'
 import { DataTable } from './data-table'
+import { NewDailyCountModal } from './new-daily-count-modal'
 
 export default function DailyCount() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- Temporary only
-  const { isFetching, isPending, error, data } = useQuery({
+  const { isFetching, data } = useQuery({
     queryKey: ['daily-count-entries'],
     queryFn: async () => await getDailyCountEntries(),
   })
+
+  const [present, dismiss] = useIonModal(NewDailyCountModal, {
+    dismiss: (data: string, role: string) => {
+      dismiss(data, role)
+    },
+  })
+
+  function handleAdd() {
+    present({
+      onWillDismiss: (event: CustomEvent<OverlayEventDetail>) => {
+        if (event.detail.role === 'confirm') {
+          console.log('You entered: ', event.detail.data)
+        }
+      },
+    })
+  }
 
   if (data === undefined) {
     return null
@@ -20,13 +49,32 @@ export default function DailyCount() {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar color="primary">
           <IonTitle>Daily Count</IonTitle>
+          <IonButtons slot="primary" collapse>
+            <IonButton onClick={handleAdd}>
+              <IonIcon icon={add} slot="icon-only" />
+            </IonButton>
+          </IonButtons>
+          {isFetching && <IonProgressBar type="indeterminate" />}
         </IonToolbar>
       </IonHeader>
 
-      <IonContent className="ion-padding">
-        <DataTable columns={columns} data={data} />
+      <IonContent>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Daily Count</IonTitle>
+            <IonButtons slot="primary">
+              <IonButton onClick={handleAdd}>
+                <IonIcon icon={add} slot="icon-only" />
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+
+        <div className="ion-padding">
+          <DataTable columns={columns} data={data} />
+        </div>
       </IonContent>
     </IonPage>
   )
