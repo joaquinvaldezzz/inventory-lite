0deb27a860a8aa5b40ba74f8090268db3f12640e
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-02-14 20:42:01 +0800

    refactor(api): optimize initialization
    - docs: Improve JSDocs

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,30 +1,49 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return -- Safe to ignore */
 import { Storage } from '@ionic/storage'
 
-/** Creates an instance of the Storage class. */
+/** Singleton instance of the Storage class. */
 const storage = new Storage()
 
+/** Tracks whether the storage has been initialized. */
+let isStorageInitialized = false
+
 /**
- * Saves a value to storage under the specified key.
+ * Ensures the storage is properly initialized before any operation.
+ *
+ * This function prevents redundant initialization by checking if the storage has already been
+ * created.
+ *
+ * @returns {Promise<void>} Resolves when storage is initialized.
+ */
+async function ensureStorageInitialized(): Promise<void> {
+  if (!isStorageInitialized) {
+    await storage.create()
+    isStorageInitialized = true
+  }
+}
+
+/**
+ * Saves a value to local storage under a specified key.
  *
  * @param {string} key - The key under which the value will be stored.
  * @param {string} value - The value to be stored.
- * @returns {Promise<void>} A promise that resolves when the value has been successfully saved to
- *   storage.
+ * @returns {Promise<void>} Resolves when the value is successfully saved.
  */
 export async function saveToStorage(key: string, value: string): Promise<void> {
-  await storage.create()
+  await ensureStorageInitialized()
   await storage.set(key, value)
 }
 
 /**
- * Retrieves a value from storage using the specified key.
+ * Retrieves a stored value by key.
  *
- * @param {string} key - The key of the value to be retrieved from storage.
- * @returns {Promise<string | null>} A promise that resolves to the value associated with the
- *   specified key, or null if the key does not exist.
+ * @param {string} key - The key associated with the stored value.
+ * @returns {Promise<string | null>} Resolves to the stored value if found, otherwise returns
+ *   `null`.
  */
 export async function getFromStorage(key: string): Promise<string | null> {
-  await storage.create()
-  return await storage.get(key)
+  await ensureStorageInitialized()
+
+  const value = (await storage.get(key)) as unknown
+
+  return typeof value === 'string' ? value : null
 }
