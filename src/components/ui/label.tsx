0c0354adae0fe0fce073cf8c1c6d9325c40b559e
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-02-27 09:16:24 +0800

    feat: export labelVariants for external usage

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 
-const labelVariants = cva(
+export const labelVariants = cva(
   'inline-block text-sm font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
 )
 
