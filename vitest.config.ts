6627260bca2e8780dbd68b74fe08ec6c8d5eaf15
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-02-27 10:44:24 +0800

    chore: configure vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,19 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { loadEnv } from 'vite'
+import { defineConfig } from 'vitest/config'
 
-export default defineConfig({})
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    root: 'src',
+    env: loadEnv('', process.cwd(), ''),
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, './src'),
+      },
+    ],
+  },
+})
