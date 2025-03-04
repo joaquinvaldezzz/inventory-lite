cccfaef07ef7893c395a469225e2d3cf6608aa67
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-03-04 14:46:42 +0800

    feat: add modal for creating new expenses

--- src/pages/app/expenses/index.tsx
+++ src/pages/app/expenses/index.tsx
@@ -7,20 +7,39 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  useIonModal,
 } from '@ionic/react'
+import type { OverlayEventDetail } from '@ionic/react/dist/types/components/react-component-lib/interfaces'
 import { add } from 'ionicons/icons'
 
 import { columns } from './columns'
 import { DataTable } from './data-table'
+import { NewExpensesModal } from './new-expenses-modal'
 
 export default function Expenses() {
+  const [present, dismiss] = useIonModal(NewExpensesModal, {
+    dismiss: (data: string, role: string) => {
+      dismiss(data, role)
+    },
+  })
+
+  function presentModal() {
+    present({
+      onWillDismiss: (event: CustomEvent<OverlayEventDetail>) => {
+        if (event.detail.role === 'confirm') {
+          // void refetch()
+        }
+      },
+    })
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar color="primary">
           <IonTitle>Expenses</IonTitle>
           <IonButtons slot="end" collapse>
-            <IonButton>
+            <IonButton onClick={presentModal}>
               <IonIcon icon={add} slot="icon-only" />
             </IonButton>
           </IonButtons>
