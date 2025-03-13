e51a267032374eaac881cbc50dd2591872e897d2
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-03-13 08:49:09 +0800

    feat: remove Suspense wrapper from root rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import { StrictMode, Suspense } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./app";
-import { Loading } from "./components/loading";
 
 const container = document.getElementById("root");
 
@@ -15,10 +14,8 @@ const root = createRoot(container);
 
 root.render(
   <StrictMode>
-    <Suspense fallback={<Loading />}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Suspense>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
