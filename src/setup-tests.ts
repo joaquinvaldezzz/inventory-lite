c354db001a9d93dbe7f40dcfe6e732fa26a47614
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-03-28 10:19:06 +0800

    test: setup Ionic React

--- src/setup-tests.ts
+++ src/setup-tests.ts
@@ -4,13 +4,30 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 
-// Mock matchmedia
-window.matchMedia =
-  window.matchMedia ||
-  function () {
-    return {
-      matches: false,
-      addListener: function () {},
-      removeListener: function () {},
-    };
+import { setupIonicReact } from "@ionic/react";
+
+setupIonicReact();
+
+window.matchMedia = function (query: string): MediaQueryList {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: function (listener: (e: MediaQueryListEvent) => void) {
+      console.warn("addListener is not implemented.");
+    },
+    removeListener: function (listener: (e: MediaQueryListEvent) => void) {
+      console.warn("removeListener is not implemented.");
+    },
+    addEventListener: function (type: string, listener: (e: Event) => void) {
+      console.warn("addEventListener is not implemented.");
+    },
+    removeEventListener: function (type: string, listener: (e: Event) => void) {
+      console.warn("removeEventListener is not implemented.");
+    },
+    dispatchEvent: function (event: Event): boolean {
+      console.warn("dispatchEvent is not implemented.");
+      return false;
+    },
   };
+};
