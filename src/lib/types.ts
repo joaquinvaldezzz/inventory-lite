bb3b3095c5d066f22ec8338a225ebb7750072ba6
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-06-20 15:26:04 +0800

    feat: enhance type definitions

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,5 @@
+/* eslint-disable max-lines -- will separate into multiple files later */
+
 /**
  * Flattens a TypeScript type for improved readability.
  *
@@ -82,32 +84,84 @@ export interface LoginData {
 /** API response for a successful login attempt. */
 export type LoginResponse = Prettify<APIResponse<LoginData>>;
 
-export interface DeliveryResponse {
-  success: boolean;
-  message: string;
-  data: {
+/** API response for a successful forgot password attempt. */
+export type ForgotPasswordResponse = Prettify<APIResponse<string>>;
+
+export interface DeliveryRecordData {
+  id: number;
+  po_no: string;
+  dr_no: string | null;
+  branch_id: number;
+  branch: string;
+  supplier_id: number;
+  supplier_name: string;
+  date_request: string;
+  date_order: string;
+  date_delivered: string;
+  payment_type_id: number;
+  payment_type: string;
+  remarks: string;
+  status_id: number;
+  status: string;
+  count: number;
+  total_amount: number;
+}
+
+/** Represents a single delivery record in a list. */
+export interface DeliveryData {
+  id: number;
+  po_no: string;
+  dr_no: string | null;
+  branch_id: number;
+  branch: string;
+  supplier_id: number;
+  supplier_name: string;
+  date_request: string;
+  date_order: string;
+  date_delivered: string;
+  payment_type_id: number;
+  payment_type: string;
+  remarks: string;
+  status_id: number;
+  status: string;
+  count: number;
+  total_amount: number;
+  items: Array<{
     id: number;
-    po_no: string;
-    dr_no: string | null;
+    purchase_id: number;
+    date_request: string;
+    date_order: string;
+    date_delivered: string;
+    date_received: string;
     branch_id: number;
     branch: string;
     supplier_id: number;
     supplier_name: string;
-    date_request: string;
-    date_order: string;
-    date_delivered: string;
-    remarks: string;
-    status_id: number;
-    status: string;
-    count: number;
+    item_id: number;
+    barcode: string;
+    raw_material: string;
+    raw_material_type_id: number;
+    raw_material_type: string;
+    sku: string;
+    packaging: string;
+    quantity_po: number;
+    quantity_actual: number;
+    unit_po: string;
+    quantity_dr: number;
+    unit_dr: string;
+    quantity: number;
+    unit: string;
+    price: number;
     total_amount: number;
-  };
+    remarks: string;
+    status: number;
+  }>;
 }
 
-export type ForgotPasswordResponse = Prettify<APIResponse<string>>;
+/** API response for a successful delivery list request. */
+export type DeliveriesResponse = Prettify<APIResponse<DeliveryData[]>>;
 
-export type DeliveryItem = DeliveryResponse["data"];
-
+/** API request to add a delivery item. */
 export interface AddDeliveryItem {
   supplier: number;
   date_request: string;
@@ -129,6 +183,7 @@ export interface AddDeliveryItem {
   }>;
 }
 
+/** API response for a successful delivery record. */
 export interface DeliveryRecord {
   id: number;
   po_no: string;
@@ -179,172 +234,169 @@ export interface DeliveryRecord {
   }>;
 }
 
-/** Represents a supplier response. */
-export interface SupplierResponse {
-  /** Indicates if the request was successful. */
-  success: boolean;
+/** API response for a single delivery record. */
+export type DeliveryRecordResponse = Prettify<APIResponse<DeliveryRecord>>;
 
-  /** Message associated with the response. */
-  message: string;
+/** Represents a supplier. */
+export interface Supplier {
+  /** Unique identifier for the supplier. */
+  id: number;
 
-  /** Array of supplier data. */
-  data: Array<{
-    /** Unique identifier for the supplier. */
-    id: number;
+  /** Name of the supplier. */
+  supplier_name: string;
 
-    /** Name of the supplier. */
-    supplier_name: string;
+  /** Last name of the supplier. */
+  lastname: string;
 
-    /** Last name of the supplier. */
-    lastname: string;
+  /** First name of the supplier. */
+  firstname: string;
 
-    /** First name of the supplier. */
-    firstname: string;
+  /** Middle name of the supplier. */
+  middlename: string;
 
-    /** Middle name of the supplier. */
-    middlename: string;
+  /** Address of the supplier. */
+  address: string;
 
-    /** Address of the supplier. */
-    address: string;
+  /** Mobile phone number of the supplier. */
+  mobile: string;
 
-    /** Mobile phone number of the supplier. */
-    mobile: string;
+  /** Landline phone number of the supplier. */
+  landline: string;
 
-    /** Landline phone number of the supplier. */
-    landline: string;
+  /** Email address of the supplier. */
+  email: string;
 
-    /** Email address of the supplier. */
-    email: string;
+  /** Tax Identification Number of the supplier. */
+  tin: string;
 
-    /** Tax Identification Number of the supplier. */
-    tin: string;
+  /**
+   * VAT type of the supplier.
+   *
+   * @default "NON-VAT"
+   */
+  vat_type: "NON-VAT";
 
-    /**
-     * VAT type of the supplier.
-     *
-     * @default "NON-VAT"
-     */
-    vat_type: "NON-VAT";
-
-    /**
-     * Currency used by the supplier.
-     *
-     * @default "PHP"
-     */
-    currency: "PHP";
-  }>;
+  /**
+   * Currency used by the supplier.
+   *
+   * @default "PHP"
+   */
+  currency: "PHP";
 }
 
-/**
- * Represents a Supplier type which is derived from the 'data' property of the SupplierResponse
- * type.
- */
-export type Supplier = SupplierResponse["data"];
+/** API response for a list of suppliers. */
+export type SuppliersResponse = Prettify<APIResponse<Supplier[]>>;
 
-export interface ItemsResponse {
-  success: boolean;
-  message: string;
-  data: Array<{
-    id: number;
-    barcode: string;
-    raw_material: string;
-    raw_material_type: number;
-    sku: string;
-    unit: string;
-    packaging: string;
-    raw_material_location: string;
-    delivery_lead_time: string;
-    minimum_stock_level: number;
-    maximum_stock_level: number;
-    initial_stock_level: number;
-  }>;
+/** Represents a single item. */
+export interface Item {
+  id: number;
+  barcode: string;
+  raw_material: string;
+  raw_material_type: number;
+  sku: string;
+  unit: string;
+  packaging: string;
+  raw_material_location: string;
+  delivery_lead_time: string;
+  minimum_stock_level: number;
+  maximum_stock_level: number;
+  initial_stock_level: number;
 }
 
-export type Items = ItemsResponse["data"];
+/** API response for a list of items. */
+export type ItemsResponse = Prettify<APIResponse<Item[]>>;
 
-export interface DailyCountResponse {
-  success: boolean;
-  message: string;
-  data: {
+/** Represents data for a daily count. */
+export interface DailyCountData {
+  id: number;
+  branch_id: number;
+  branch: string;
+  branch_name: string;
+  raw_material_type_id: number;
+  raw_material_type: string;
+  date: string;
+  timestamp: string;
+  items: Array<{
     id: number;
+    inventory_count_id: number;
     branch_id: number;
     branch: string;
     branch_name: string;
+    date: string;
     raw_material_type_id: number;
     raw_material_type: string;
-    date: string;
-    timestamp: string;
-  };
-}
-
-export type DailyCountData = DailyCountResponse["data"];
-
-export interface CategoriesResponse {
-  success: boolean;
-  message: string;
-  data: {
-    id: number;
-    raw_material_type: string;
-  };
-}
-
-export type Categories = CategoriesResponse["data"];
-
-export interface IngredientsResponse {
-  success: boolean;
-  message: string;
-  data: {
-    id: number;
+    item_id: number;
     barcode: string;
-    raw_material: string;
-    raw_material_type: number;
-    sku: string;
+    item: string;
     unit: string;
-    packaging: string;
-    raw_material_location: string;
-    delivery_lead_time: string;
-    minimum_stock_level: number;
-    maximum_stock_level: number;
-    initial_stock_level: number;
-    days_stock: null;
-    ChartAccountID: null;
-  };
+    count: number;
+  }>;
 }
 
-export type Ingredients = IngredientsResponse["data"];
+/** API response for daily count data. */
+export type DailyCountResponse = Prettify<APIResponse<DailyCountData>>;
 
-export interface DailyCountRecordResponse {
-  success: boolean;
-  message: string;
-  data: {
+/** Represents a single category. */
+export interface Category {
+  id: number;
+  raw_material_type: string;
+}
+
+/** API response for a list of categories. */
+export type CategoriesResponse = Prettify<APIResponse<Category[]>>;
+
+/** Represents a single ingredient. */
+export interface Ingredient {
+  id: number;
+  barcode: string;
+  raw_material: string;
+  raw_material_type: number;
+  sku: string;
+  unit: string;
+  packaging: string;
+  raw_material_location: string;
+  delivery_lead_time: string;
+  minimum_stock_level: number;
+  maximum_stock_level: number;
+  initial_stock_level: number;
+  days_stock: null;
+  ChartAccountID: null;
+}
+
+/** API response for a list of ingredients. */
+export type IngredientsResponse = Prettify<APIResponse<Ingredient[]>>;
+
+/** API response for a successful daily count record attempt. */
+export interface DailyCountRecord {
+  id: number;
+  branch_id: number;
+  branch: string;
+  branch_name: string;
+  raw_material_type_id: number;
+  raw_material_type: string;
+  date: string;
+  timestamp: string;
+  items: Array<{
     id: number;
+    inventory_count_id: number;
     branch_id: number;
     branch: string;
     branch_name: string;
+    date: string;
     raw_material_type_id: number;
     raw_material_type: string;
-    date: string;
-    timestamp: string;
-    items: Array<{
-      id: number;
-      inventory_count_id: number;
-      branch_id: number;
-      branch: string;
-      branch_name: string;
-      date: string;
-      raw_material_type_id: number;
-      raw_material_type: string;
-      item_id: number;
-      barcode: string;
-      item: string;
-      unit: string;
-      count: number;
-    }>;
-  };
+    item_id: number;
+    barcode: string;
+    item: string;
+    unit: string;
+    count: number;
+  }>;
 }
 
-export type DailyCountRecord = DailyCountRecordResponse["data"];
+/** API response for a single daily count record. */
+export type DailyCountRecordResponse = Prettify<APIResponse<DailyCountRecord>>;
 
+/** API response for a successful waste attempt. */
 export interface WasteData {
   id: number;
   branch_id: number;
@@ -356,8 +408,10 @@ export interface WasteData {
   date: string;
 }
 
-export type WasteResponse = Prettify<APIResponse<WasteData>>;
+/** API response for waste data. */
+export type WasteResponse = Prettify<APIResponse<WasteData[]>>;
 
+/** API response for a successful waste record attempt. */
 export interface WasteRecordData {
   id: number;
   branch_id: number;
@@ -386,8 +440,10 @@ export interface WasteRecordData {
   }>;
 }
 
-export type WasteRecordResponse = Prettify<APIResponse<WasteRecordData[]>>;
+/** API response for waste record data. */
+export type WasteRecordResponse = Prettify<APIResponse<WasteRecordData>>;
 
+/** API response for a successful employee attempt. */
 export interface EmployeeData {
   EmployeeID: string;
   CompanyID: string;
@@ -469,8 +525,10 @@ export interface EmployeeData {
   NDPay: string;
 }
 
+/** API response for a list of employees. */
 export type EmployeesResponse = Prettify<APIResponse<EmployeeData[]>>;
 
+/** API response for a successful expenses record attempt. */
 export interface ExpensesRecordData {
   CompanyID: string;
   PurchaseID: number;
@@ -496,33 +554,61 @@ export interface ExpensesRecordData {
   Status: string;
   TotalCR: number;
   TotalDR: number;
-  items: [
-    {
-      ID: number;
-      PurchaseID: number;
-      PONo: string;
-      Particulars: string;
-      item_pricelist_id: null;
-      GSC: null;
-      ChartAccountID: number;
-      Quantity: number;
-      Cost: number;
-      Amount: number;
-      TotalCR: number;
-      TotalDR: number;
-      TotalStatus: string;
-      WTax: null;
-      InputVat: null;
-      WTaxPayment: null;
-      DepartmentOffice: null;
-      Exempt: null;
-      ZeroRated: null;
-      GovtSales: null;
-      ATC: null;
-      TaxType: string;
-      vatable_sales: null;
-    },
-  ];
+  items: Array<{
+    ID: number;
+    PurchaseID: number;
+    PONo: string;
+    Particulars: string;
+    item_pricelist_id: null;
+    GSC: null;
+    ChartAccountID: number;
+    Quantity: number;
+    Cost: number;
+    Amount: number;
+    TotalCR: number;
+    TotalDR: number;
+    TotalStatus: string;
+    WTax: null;
+    InputVat: null;
+    WTaxPayment: null;
+    DepartmentOffice: null;
+    Exempt: null;
+    ZeroRated: null;
+    GovtSales: null;
+    ATC: null;
+    TaxType: string;
+    vatable_sales: null;
+  }>;
 }
 
+/** API response for a list of expense records. */
 export type ExpensesRecordsResponse = Prettify<APIResponse<ExpensesRecordData[]>>;
+
+/** Represents a single reason for waste. */
+export interface Reason {
+  id: number;
+  reason: string;
+}
+
+/** API response for a list of reasons. */
+export type ReasonsResponse = Prettify<APIResponse<Reason[]>>;
+
+/** Represents a single employee for waste records. */
+export interface WasteEmployee {
+  EmployeeID: string;
+  FullName: string;
+}
+
+/** API response for a list of employees for waste records. */
+export type WasteEmployeesResponse = Prettify<APIResponse<WasteEmployee[]>>;
+
+/** Represents a single item for waste records. */
+export interface WasteItem {
+  id: number;
+  item: string;
+  barcode: string;
+  unit: string;
+}
+
+/** API response for a list of items for waste records. */
+export type WasteItemsResponse = Prettify<APIResponse<WasteItem[]>>;
