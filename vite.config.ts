6a5a537022752c66ae6f83cfa25dcf90f8e6b42e
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2026-09-12 09:19:37 +0800

    feat: install vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 // import legacy from '@vitejs/plugin-legacy'
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [VitePWA({ registerType: "autoUpdate" }), react(), tailwindcss()],
   resolve: {
     alias: {
       "@/root": path.resolve(__dirname, ""),
